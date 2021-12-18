90b18ef6d914227a4167042dcc9fb694cb9ca3bd
WEI.LIN <chiwei991814@gmail.com>
2021-12-18 14:30:48 +0800

    feat: add industries API

---
+++ pages/api/surveys/company/industries/format.page.js
@@ -0,0 +1,22 @@
+import nc from "next-connect";
+import Surveys from "models/Surveys";
+import db from "utils/db";
+import { getFormatIndustryGroup } from "utils/common";
+const handler = nc();
+
+handler.get(async (req, res) => {
+  await db.connect();
+  const groups = await Surveys.aggregate([
+    {
+      $group: { _id: "$company.industry", count: { $sum: 1 } },
+    },
+    {
+      $sort: { count: -1 },
+    },
+  ]);
+  await db.disconnect();
+
+  res.send(getFormatIndustryGroup(groups));
+});
+
+export default handler;
